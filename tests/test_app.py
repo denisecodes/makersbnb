9d4c64350d04ad460a5691b803b795acb6cc424d
Neil Studd <neil.studd@makers.tech>
2023-05-24 15:20:24 +0100

    [SLUG] Transferring changes from Slug as of 2023-05-24 15:20:22 +0100.

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Page, expect
+
+# Tests for your routes go here
+
+"""
+We can render the index page
+"""
+def test_get_index(page, test_web_address):
+    # We load a virtual browser and navigate to the /index page
+    page.goto(f"http://{test_web_address}/index")
+
+    # We look at the <h1> tag
+    strong_tag = page.locator("h1")
+
+    # We assert that it has the text "This is the homepage."
+    expect(strong_tag).to_have_text("This is the homepage.")
