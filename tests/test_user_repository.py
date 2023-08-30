e9fba6e1fdc579e92e508db844353568963cfdfb
Emily-RC <emilycowan1993@gmail.com>
2023-08-30 13:18:39 +0100

    Implemented User Repository Class

--- tests/test_user_repository.py
+++ tests/test_user_repository.py
@@ -9,3 +9,11 @@ def test_check_password_and_username(db_connection):
     db_connection.seed('seeds/users.sql')
     repository = UserRepository(db_connection)
     assert repository.validate_login('email1@gmail.com','12345') == True
+
+def test_check_login_is_false(db_connection):
+    db_connection.seed('seeds/users.sql')
+    repository = UserRepository(db_connection)
+    assert repository.validate_login('random@gmail.com','12347') == False
+
+
+    
