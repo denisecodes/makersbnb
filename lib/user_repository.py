90098e05cf625b9e9bf317ad58006c1caf14852a
Emily-RC <emilycowan1993@gmail.com>
2023-08-31 16:39:25 +0100

    Debugged login_merge

--- lib/user_repository.py
+++ lib/user_repository.py
@@ -2,11 +2,11 @@ from lib.user import User
 
 class UserRepository():
     def __init__(self, connection):
-        self.connection = connection
+        self._connection = connection
         
 
     def validate_login(self, email_address, user_password):
-        count = self.connection.execute(
+        count = self._connection.execute(
             'SELECT COUNT(id) FROM users WHERE email_address = (%s) AND user_password = (%s)', [email_address, user_password])
         value = count[0]["count"]
         if value == 1:
--- seed_test_database.py
+++ seed_test_database.py
@@ -1,7 +1,7 @@
 from lib.database_connection import DatabaseConnection
 # Run this file to reset your database using the seeds
 # ; pipenv run python seed_dev_database.py
-connection = DatabaseConnection(test_mode=False)
+connection = DatabaseConnection(test_mode=True)
 connection.connect()
 connection.seed("seeds/seed_spaces.sql")
 connection.seed("seeds/users.sql")
