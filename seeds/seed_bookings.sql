7570c7387adffa3415ad23bbc0aaba5385eebe6e
Alexander Wilson <apwilson97@gmail.com>
2023-09-01 14:07:16 +0100

    Updated availability feature to account for months with several spaces booked in

--- seeds/seed_bookings.sql
+++ seeds/seed_bookings.sql
@@ -12,3 +12,7 @@ INSERT INTO bookings (booked_month, spaces_id) VALUES ('March', 1);
 INSERT INTO bookings (booked_month, spaces_id) VALUES ('February', 3);
 INSERT INTO bookings (booked_month, spaces_id) VALUES ('November', 5);
 INSERT INTO bookings (booked_month, spaces_id) VALUES ('July', 1);
+INSERT INTO bookings (booked_month, spaces_id) VALUES ('March', 2);
+INSERT INTO bookings (booked_month, spaces_id) VALUES ('March', 4);
+INSERT INTO bookings (booked_month, spaces_id) VALUES ('November', 6);
+INSERT INTO bookings (booked_month, spaces_id) VALUES ('November', 2);
